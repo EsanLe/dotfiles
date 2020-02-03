630732e57d76a8bbff02a650230581ba89d804d6
eiddlechen1008@gmail.com <eddie@ali-119721.local>
2020-02-03 10:17:46 +0800

    Refactor 10.15

---
+++ iterm2/install.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors > /tmp/SolarizedDark.itermcolors 
+open /tmp/SolarizedDark.itermcolors
